3e01e902a11d75f18bc291b3e178026812aeb2b3
Mike North <northm@yahoo-inc.com>
2015-07-27 11:05:07 -0700

    private block comment

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -5,6 +5,8 @@ import config from './config/environment';
 var EMBER_VERSION_REGEX = /^([0-9]+)\.([0-9]+)\.([0-9]+)(?:(?:\-(alpha|beta)\.([0-9]+)(?:\.([0-9]+))?)?)?(?:\+(canary))?(?:\.([0-9abcdef]+))?(?:\-([A-Za-z0-9\.\-]+))?(?:\+([A-Za-z0-9\.\-]+))?$/;
 
 /**
+ * @private
+ * 
  * VERSION_INFO[i] is as follows:
  *
  * 0  complete version string
@@ -17,7 +19,6 @@ var EMBER_VERSION_REGEX = /^([0-9]+)\.([0-9]+)\.([0-9]+)(?:(?:\-(alpha|beta)\.([
  * 7  canary (optional: "canary", or undefined for stable releases)
  * 8  SHA (optional)
  */
-
 var VERSION_INFO = EMBER_VERSION_REGEX.exec(Ember.VERSION);
 var IS_PRE_17 = parseInt(VERSION_INFO[1], 10) < 2 && parseInt(VERSION_INFO[2], 10) < 7;
 
