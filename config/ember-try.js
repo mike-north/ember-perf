f4533fc095838b402b85ed6773f64bf037cae7bc
Mike North <michael.l.north@gmail.com>
2015-07-27 01:08:03 -0700

    Build pipeline

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,35 +1,54 @@
 module.exports = {
   scenarios: [
     {
-      name: 'default',
-      dependencies: { }
-    },
-    {
-      name: 'ember-release',
+      name: 'ember-1.8',
       dependencies: {
-        'ember': 'components/ember#release'
+        ember: '~1.8.1',
+        handlebars: '~1.3.0'
       },
       resolutions: {
-        'ember': 'release'
+        ember: '~1.8.1',
+        handlebars: '~1.3.0'
       }
     },
     {
-      name: 'ember-beta',
+      name: 'ember-1.9',
       dependencies: {
-        'ember': 'components/ember#beta'
+        ember: '~1.9.1',
+        handlebars: '~2.0.0'
       },
       resolutions: {
-        'ember': 'beta'
-      }
-    },
-    {
-      name: 'ember-canary',
-      dependencies: {
-        'ember': 'components/ember#canary'
-      },
-      resolutions: {
-        'ember': 'canary'
+        ember: '~1.9.1',
+        handlebars: '~2.0.0'
       }
     }
+    // ,
+    // {
+    //   name: 'ember-release',
+    //   dependencies: {
+    //     'ember': 'components/ember#release'
+    //   },
+    //   resolutions: {
+    //     'ember': 'release'
+    //   }
+    // },
+    // {
+    //   name: 'ember-beta',
+    //   dependencies: {
+    //     'ember': 'components/ember#beta'
+    //   },
+    //   resolutions: {
+    //     'ember': 'beta'
+    //   }
+    // },
+    // {
+    //   name: 'ember-canary',
+    //   dependencies: {
+    //     'ember': 'components/ember#canary'
+    //   },
+    //   resolutions: {
+    //     'ember': 'canary'
+    //   }
+    // }
   ]
 };
