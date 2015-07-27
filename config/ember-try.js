a0560f803928dd05bd056049f3a5b7f9092378ef
Mike North <michael.l.north@gmail.com>
2015-07-27 01:50:09 -0700

    Test against 1.11, 1.12, 1.13, 2.0

--- config/ember-try.js
+++ config/ember-try.js
@@ -43,34 +43,55 @@ module.exports = {
         ember: '~1.10.0',
         handlebars: null
       }
+    },
+    {
+      name: 'ember-1.11',
+      dependencies: {
+        ember: '~1.11.0',
+        handlebars: null
+      },
+      resolutions: {
+        ember: '~1.11.0',
+        handlebars: null
+      }
+    },
+    {
+      name: 'ember-1.12',
+      dependencies: {
+        ember: '~1.12.0',
+        handlebars: null
+      },
+      resolutions: {
+        ember: '~1.12.0',
+        handlebars: null
+      }
+    },
+    {
+      name: 'ember-release',
+      dependencies: {
+        'ember': 'components/ember#release'
+      },
+      resolutions: {
+        'ember': 'release'
+      }
+    },
+    {
+      name: 'ember-beta',
+      dependencies: {
+        'ember': 'components/ember#beta'
+      },
+      resolutions: {
+        'ember': 'beta'
+      }
+    },
+    {
+      name: 'ember-canary',
+      dependencies: {
+        'ember': 'components/ember#canary'
+      },
+      resolutions: {
+        'ember': 'canary'
+      }
     }
-    // ,
-    // {
-    //   name: 'ember-release',
-    //   dependencies: {
-    //     'ember': 'components/ember#release'
-    //   },
-    //   resolutions: {
-    //     'ember': 'release'
-    //   }
-    // },
-    // {
-    //   name: 'ember-beta',
-    //   dependencies: {
-    //     'ember': 'components/ember#beta'
-    //   },
-    //   resolutions: {
-    //     'ember': 'beta'
-    //   }
-    // },
-    // {
-    //   name: 'ember-canary',
-    //   dependencies: {
-    //     'ember': 'components/ember#canary'
-    //   },
-    //   resolutions: {
-    //     'ember': 'canary'
-    //   }
-    // }
   ]
 };
