c9a49e447393a60d3939e32a543a52692bc706c4
Mike North <michael.l.north@gmail.com>
2015-07-28 00:47:08 -0700

    Only deploy to gh-pages in ember-release build

--- lib/try-generate-pages.sh
+++ lib/try-generate-pages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Attempting to generate pages"
-if [[ "${TRAVIS_PULL_REQUEST}" = "false" && "${TRAVIS_BRANCH}" == 'master' ]]; then
+if [[ "${TRAVIS_PULL_REQUEST}" = "false" && "${TRAVIS_BRANCH}" == 'master' && "${EMBER_TRY_SCENARIO}" == 'ember-release' ]]; then
   ember github-pages:commit --message "New release" --branch "gh-pages"
   git push deploy gh-pages 2>&1 >/dev/null
 else
-  echo "Not a main build -- no deploy"
+  echo "Not a master build -- no deploy"
 fi
