2ad207e66d6f0e823b58a798db48285af8375eff
Mike North <michael.l.north@gmail.com>
2015-07-28 00:59:17 -0700

    CD fix

--- lib/try-generate-pages.sh
+++ lib/try-generate-pages.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo "Attempting to generate pages"
 if [[ "${TRAVIS_PULL_REQUEST}" = "false" && "${TRAVIS_BRANCH}" == 'master' && "${EMBER_TRY_SCENARIO}" == 'ember-release' ]]; then
+  rm bower.json.ember-try
+  git add -A .
+  git commit -m "temp"
   ember github-pages:commit --message "New release" --branch "gh-pages"
   git push deploy gh-pages 2>&1 >/dev/null
 else
