a8d6f73d9e30cf2ee21644a4792003a1bba3e0a3
Mike North <michael.l.north@gmail.com>
2015-07-28 01:15:40 -0700

    a basic test

---
+++ tests/acceptance/companies-test.js
@@ -0,0 +1,31 @@
+import Ember from 'ember';
+import { module, test } from 'qunit';
+import startApp from '../../tests/helpers/start-app';
+
+let application;
+
+module('Acceptance | companies', {
+  beforeEach() {
+    application = startApp();
+  },
+
+  afterEach() {
+    Ember.run(application, 'destroy');
+  }
+});
+
+test('visiting /companies', function(assert) {
+  visit('/companies');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/companies');
+    assert.equal(find('.companies-list li').length, 1, 'One company in list');
+    assert.equal(find('.companies-list .company')[0].innerText, 'Yahoo Inc', 'Yahoo is first company');
+  });
+
+  click('.companies-list .company:first-child a');
+
+  andThen(function () {
+    assert.equal(currentURL(), '/companies/1/buildings');
+  });
+});
