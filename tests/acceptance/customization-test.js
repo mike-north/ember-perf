a9dca4b34f854bb9b986984d536f26b07c8473f1
Mike North <northm@yahoo-inc.com>
2015-08-19 15:20:08 -0700

    Drill-in test

--- tests/acceptance/customization-test.js
+++ tests/acceptance/customization-test.js
@@ -7,18 +7,32 @@ let application;
 module('Acceptance | drilling in', {
   beforeEach() {
     application = startApp();
+    application.perfService = application.__container__.lookup('service:ember-perf');
   },
 
   afterEach() {
     Ember.run(application, 'destroy');
+    application = null;
   }
 });
 
 test('Drilling in', function(assert) {
+  let dataCount = 0;
+  application.perfService.on('transitionComplete', () => {
+    dataCount++;
+  });
+
   visit('/company/1/buildings');
 
   andThen(function() {
+    assert.equal(dataCount, 1, 'Only one event has been fired for the initial load');
     assert.equal(currentURL(), '/company/1/buildings');
+  });
+
+  click('ul.buildings-list li:first-child a');
+
+  andThen(function() {
+    assert.equal(dataCount, 2, 'Only one event has been fired for a drill-in');
   });
 
 });
