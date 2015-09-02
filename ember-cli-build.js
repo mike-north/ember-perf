efd1c8fd49b5a081d48b12f0186afb0d3f2c1759
Mike North <northm@yahoo-inc.com>
2015-09-02 16:05:34 -0700

    save

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,7 +3,11 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
-    // Add options here
+    vendorFiles: {
+      'handlebars.js': {
+        production: 'bower_components/handlebars/handlebars.js'
+      }
+    }
   });
 
   /*
