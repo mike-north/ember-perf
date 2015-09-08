3cf938c2ef998d8529006de64ad41629adfa5abd
Robert Jackson <robert.w.jackson@me.com>
2015-09-08 15:44:03 -0400

    Allow opting out of view instrumentation hooks.
    The instrumentation of `render` / `rerender` events is MEGA SLOW right
    now (which we should definitely fix). As an example, running render 10
    times with https://github.com/ef4/initial-render-perf:
    
    * 154ms - mean with `logViewEvents` set to `false`
    * 1437ms - mean with `logViewEvents` set to `true` (the current default)

--- app/initializers/ember-perf.js
+++ app/initializers/ember-perf.js
@@ -2,12 +2,13 @@ import EmberPerfService from 'ember-perf/services/ember-perf';
 import RouterExt from 'ember-perf/ext/router';
 import RouteExt from 'ember-perf/ext/route';
 import config from '../config/environment';
+import instanceInitializer from '../instance-initializers/ember-perf';
 
 const {
   Router, Route
 } = Ember;
 
-function injectServiceOntoFactories(emberPerf, container, application) {
+function injectServiceOntoFactories(emberPerf, application) {
   const {
     injectionFactories
   } = emberPerf;
@@ -26,25 +27,21 @@ function injectServiceOntoFactories(emberPerf, container, application) {
 export function initialize() {
   const application = arguments[1] || arguments[0];
   const container = application.__container__;
-  
+
   const {
     emberPerf
   } = config;
-  
-  injectServiceOntoFactories(emberPerf, container, application);
+
+  injectServiceOntoFactories(emberPerf, application);
 
   Route.reopen(RouteExt);
   Router.reopen(RouterExt);
 
-  Ember.subscribe("render", {
-    before(name, timestamp, payload) {
-      container.lookup('service:ember-perf').renderBefore(name, timestamp, payload);
-    },
-    after(name, timestamp, payload) {
-      container.lookup('service:ember-perf').renderAfter(name, timestamp, payload);
-    }
-  });
-
+  // instance initializers were available as of Ember 1.12, this
+  // runs the instance initializer manually if needed
+  if (!application.instanceInitializer) {
+    instanceInitializer.initialize(container);
+  }
 };
 
 export default {
