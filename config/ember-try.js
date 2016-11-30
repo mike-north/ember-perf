6d5cf56db1600723d429112acc3596b1431a7f8b
Dave Laird <kiwiupover@gmail.com>
2016-11-29 22:22:15 -0800

    Updated ember, ember-data and ember-cli to 2.10.0

--- config/ember-try.js
+++ config/ember-try.js
@@ -123,6 +123,61 @@ module.exports = {
       }
     },
     {
+      name: 'ember-2.6',
+      bower: {
+        dependencies: {
+          'ember': '~2.6.0'
+        },
+        resolutions: {
+          'ember': '~2.6.0'
+        }
+      }
+    },
+    {
+      name: 'ember-2.7',
+      bower: {
+        dependencies: {
+          'ember': '~2.7.0'
+        },
+        resolutions: {
+          'ember': '~2.7.0'
+        }
+      }
+    },
+    {
+      name: 'ember-2.8',
+      bower: {
+        dependencies: {
+          'ember': '~2.8.0'
+        },
+        resolutions: {
+          'ember': '~2.8.0'
+        }
+      }
+    },
+    {
+      name: 'ember-2.9',
+      bower: {
+        dependencies: {
+          'ember': '~2.9.0'
+        },
+        resolutions: {
+          'ember': '~2.9.0'
+        }
+      }
+    },
+    {
+      name: 'ember-2.10',
+      bower: {
+        dependencies: {
+          'ember': '~2.10.0'
+        },
+        resolutions: {
+          'ember': '~2.10.0'
+        }
+      }
+    },
+    {
       name: 'ember-release',
       bower: {
         dependencies: {
